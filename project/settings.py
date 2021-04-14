c43da2dc6e941c9cb6e6415f5c0a424b2d4f63f0
amiftekher <amiftekher@gmail.com>
2021-04-14 17:31:47 +0600

    Cloudinary storage added

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = '*=5ux0rl7&#%3i_vqib@n4a(_%^@l5d%1&+dbz_+$2-y#b4bjx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['image-upload-nabil.herokuapp.com']
+ALLOWED_HOSTS = ['image-upload-nabil.herokuapp.com','127.0.0.1','localhost']
+
 
 # Application definition
 
